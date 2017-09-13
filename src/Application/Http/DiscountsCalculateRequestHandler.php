a347d58bd3e3ed21234ba0f366e239279d11bc55
Bram Van der Sype <bram.vandersype@gmail.com>
2017-09-13 11:54:10 +0200

    Convert request to order object

--- src/Application/Http/DiscountsCalculateRequestHandler.php
+++ src/Application/Http/DiscountsCalculateRequestHandler.php
@@ -2,6 +2,9 @@
 
 namespace Brammm\TLDiscounts\Application\Http;
 
+use Brammm\TLDiscounts\Domain\Order\Item;
+use Brammm\TLDiscounts\Domain\Order\Order;
+use Brammm\TLDiscounts\Domain\Order\Price;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -9,6 +12,25 @@ class DiscountsCalculateRequestHandler
 {
     public function __invoke(Request $request, Response $response)
     {
+        // TODO: Add validation that these parameters are set
+        $items = [];
+        foreach ($request->getParsedBody()['items'] as $item) {
+            $items[] = new Item(
+                $item['product-id'],
+                (int)$item['quantity'],
+                new Price((float)$item['unit-price']),
+                new Price((float)$item['total'])
+            );
+        }
+
+        $order = new Order(
+            (int)$request->getParsedBody()['id'],
+            (int)$request->getParsedBody()['customer-id'],
+            $items,
+            new Price((float)$request->getParsedBody()['total'])
+        );
+
+
         return $response;
     }
 }
