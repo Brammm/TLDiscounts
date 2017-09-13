0005be1e8d9c1f8bf3de99e7079ce55c553d8567
Bram Van der Sype <bram.vandersype@gmail.com>
2017-09-13 14:33:59 +0200

    Fix spacing

--- tests/Functional/PostDiscountsCalculateTest.php
+++ tests/Functional/PostDiscountsCalculateTest.php
@@ -10,179 +10,179 @@ class PostDiscountsCalculateTest extends AppTestCase
     public function testOrderOne()
     {
         $response = $this->request('POST', '/discounts.calculate', [
-            'id'=> '1',
-            'customer-id'=> '1',
-            'items'=> [
+            'id' => '1',
+            'customer-id' => '1',
+            'items' => [
                 [
-                    'product-id'=> 'B102',
-                    'quantity'=> '10',
-                    'unit-price'=> '4.99',
-                    'total'=> '49.90',
+                    'product-id' => 'B102',
+                    'quantity' => '10',
+                    'unit-price' => '4.99',
+                    'total' => '49.90',
                 ],
             ],
-            'total'=> '49.90',
+            'total' => '49.90',
         ]);
 
         $data = json_decode($response->getBody(), true);
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals([
-            'id'=> '1',
-            'customer-id'=> '1',
-            'items'=> [
+            'id' => '1',
+            'customer-id' => '1',
+            'items' => [
                 [
-                    'product-id'=> 'B102',
-                    'quantity'=> '12',
-                    'unit-price'=> '4.99',
-                    'total'=> '49.90',
+                    'product-id' => 'B102',
+                    'quantity' => '12',
+                    'unit-price' => '4.99',
+                    'total' => '49.90',
                     'applied-discount' => 'BuyFiveOneFreeExtra',
                 ],
             ],
             'applied-discount' => null,
-            'total'=> '49.90',
+            'total' => '49.90',
         ], $data);
     }
 
     public function testOrderTwo()
     {
         $response = $this->request('POST', '/discounts.calculate', [
-            'id'=> '2',
-            'customer-id'=> '2',
-            'items'=> [
+            'id' => '2',
+            'customer-id' => '2',
+            'items' => [
                 [
-                    'product-id'=> 'B102',
-                    'quantity'=> '5',
-                    'unit-price'=> '4.99',
-                    'total'=> '24.95',
+                    'product-id' => 'B102',
+                    'quantity' => '5',
+                    'unit-price' => '4.99',
+                    'total' => '24.95',
                 ],
             ],
-            'total'=> '24.95',
+            'total' => '24.95',
         ]);
 
         $data = json_decode($response->getBody(), true);
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals([
-            'id'=> '2',
-            'customer-id'=> '2',
-            'items'=> [
+            'id' => '2',
+            'customer-id' => '2',
+            'items' => [
                 [
-                    'product-id'=> 'B102',
-                    'quantity'=> '6',
-                    'unit-price'=> '4.99',
-                    'total'=> '24.95',
+                    'product-id' => 'B102',
+                    'quantity' => '6',
+                    'unit-price' => '4.99',
+                    'total' => '24.95',
                     'applied-discount' => 'BuyFiveOneFreeExtra',
                 ],
             ],
             'applied-discount' => 'HighRevenueCustomer',
-            'total'=> '22.46',
+            'total' => '22.46',
         ], $data);
     }
 
     public function testOrderThree()
     {
         $response = $this->request('POST', '/discounts.calculate', [
-            'id'=> '3',
-            'customer-id'=> '3',
-            'items'=> [
+            'id' => '3',
+            'customer-id' => '3',
+            'items' => [
                 [
-                    'product-id'=> 'A101',
-                    'quantity'=> '2',
-                    'unit-price'=> '9.75',
-                    'total'=> '19.50',
+                    'product-id' => 'A101',
+                    'quantity' => '2',
+                    'unit-price' => '9.75',
+                    'total' => '19.50',
                 ],
                 [
-                    'product-id'=> 'A102',
-                    'quantity'=> '1',
-                    'unit-price'=> '49.50',
-                    'total'=> '49.50',
+                    'product-id' => 'A102',
+                    'quantity' => '1',
+                    'unit-price' => '49.50',
+                    'total' => '49.50',
                 ],
             ],
-            'total'=> '69.00',
+            'total' => '69.00',
         ]);
 
         $data = json_decode($response->getBody(), true);
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals([
-            'id'=> '3',
-            'customer-id'=> '3',
-            'items'=> [
+            'id' => '3',
+            'customer-id' => '3',
+            'items' => [
                 [
-                    'product-id'=> 'A101',
-                    'quantity'=> '2',
-                    'unit-price'=> '9.75',
-                    'total'=> '15.60',
+                    'product-id' => 'A101',
+                    'quantity' => '2',
+                    'unit-price' => '9.75',
+                    'total' => '15.60',
                     'applied-discount' => 'TwentyOffCheapest',
                 ],
                 [
-                    'product-id'=> 'A102',
-                    'quantity'=> '1',
-                    'unit-price'=> '49.50',
-                    'total'=> '49.50',
+                    'product-id' => 'A102',
+                    'quantity' => '1',
+                    'unit-price' => '49.50',
+                    'total' => '49.50',
                     'applied-discount' => null,
                 ],
             ],
             'applied-discount' => null,
-            'total'=> '65.10',
+            'total' => '65.10',
         ], $data);
     }
 
     public function testOrderFour()
     {
         $response = $this->request('POST', '/discounts.calculate', [
-            'id'=> '4',
-            'customer-id'=> '2',
-            'items'=> [
+            'id' => '4',
+            'customer-id' => '2',
+            'items' => [
                 [
-                    'product-id'=> 'B102',
-                    'quantity'=> '5',
-                    'unit-price'=> '4.99',
-                    'total'=> '24.95',
+                    'product-id' => 'B102',
+                    'quantity' => '5',
+                    'unit-price' => '4.99',
+                    'total' => '24.95',
                 ],
                 [
-                    'product-id'=> 'A101',
-                    'quantity'=> '2',
-                    'unit-price'=> '9.75',
-                    'total'=> '19.50',
+                    'product-id' => 'A101',
+                    'quantity' => '2',
+                    'unit-price' => '9.75',
+                    'total' => '19.50',
                 ],
                 [
-                    'product-id'=> 'A102',
-                    'quantity'=> '1',
-                    'unit-price'=> '49.50',
-                    'total'=> '49.50',
+                    'product-id' => 'A102',
+                    'quantity' => '1',
+                    'unit-price' => '49.50',
+                    'total' => '49.50',
                 ],
             ],
-            'total'=> '69.00',
+            'total' => '69.00',
         ]);
 
         $data = json_decode($response->getBody(), true);
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals([
-            'id'=> '4',
-            'customer-id'=> '2',
-            'items'=> [
+            'id' => '4',
+            'customer-id' => '2',
+            'items' => [
                 [
-                    'product-id'=> 'B102',
-                    'quantity'=> '6',
-                    'unit-price'=> '4.99',
-                    'total'=> '24.95',
+                    'product-id' => 'B102',
+                    'quantity' => '6',
+                    'unit-price' => '4.99',
+                    'total' => '24.95',
                     'applied-discount' => 'BuyFiveOneFreeExtra',
                 ],
                 [
-                    'product-id'=> 'A101',
-                    'quantity'=> '2',
-                    'unit-price'=> '9.75',
-                    'total'=> '15.60',
+                    'product-id' => 'A101',
+                    'quantity' => '2',
+                    'unit-price' => '9.75',
+                    'total' => '15.60',
                     'applied-discount' => 'TwentyOffCheapest',
                 ],
                 [
-                    'product-id'=> 'A102',
-                    'quantity'=> '1',
-                    'unit-price'=> '49.50',
-                    'total'=> '49.50',
+                    'product-id' => 'A102',
+                    'quantity' => '1',
+                    'unit-price' => '49.50',
+                    'total' => '49.50',
                     'applied-discount' => null,
                 ],
             ],
             'applied-discount' => 'HighRevenueCustomer',
-            'total'=> '90.05',
+            'total' => '90.05',
         ], $data);
     }
 }
